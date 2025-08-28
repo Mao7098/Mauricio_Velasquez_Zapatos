6206b45bfa1c72cb3e56add37a7b7c19bb8c5aa4
Mauro <mauriciiov7098@gmail.com>
2025-08-28 08:42:02 -0500

    Añado clases y pruebo que funcione

--- Zapatos/Zapatos/Program.cs
+++ Zapatos/Zapatos/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 Console.WriteLine("Hello, World!");
+
+public class Marcas
+{
+    public int Id;
+    public string? Nombre;
+
+    
+}
+
+public class Zapatos
+{
+    
+    public string? Nombre;
+    public int Precio;
+
+    public Marcas? Marca;
+}
+
+
+public class Ventas
+{
+    public Personas? Cliente;
+
+    public Zapatos? Zapato;
+
+}
+
+public class Personas
+{
+    public int Id;
+    public string? Nombre;
+}
